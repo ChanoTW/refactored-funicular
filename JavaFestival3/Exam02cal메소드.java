71829d49ec351fac06409bb1da1ee87743cb57c2
ChanoTW <95010759+ChanoTW@users.noreply.github.com>
2021-12-05 16:36:56 +0900

    Update Exam02cal메소드.java

--- JavaFestival3/Exam02cal메소드.java
+++ JavaFestival3/Exam02cal메소드.java
@@ -4,26 +4,28 @@ public class Exam02cal메소드 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.print("첫번째 정수 >>");
-		int num1 = sc.nextInt();
-		System.out.print("두번째 정수 >>");
-		int num2 = sc.nextInt();
-		
+		int num1 = 50;
+		int num2 = 15;
 		char op = '-';
 		
 		System.out.println(cal(num1,num2,op));
-
+		
 	}
-	
-	
-	public static int cal(int x1, int x2, char x3) {
-		return x1 - x2;
+	public static int cal(int a, int b, char c) {
 		
-		
-		
-		
+		if(c == '+') {
+			return a + b;
+		} else if (c == '-') {
+			return a - b;
+		}
+		else if (c == '*') {
+			return a * b;
+		}
+		else if (c == '/') {
+			return a / b;
+		}
 		
 	}
 
 }
+
